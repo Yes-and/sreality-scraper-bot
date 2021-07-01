1640a1d6faafb51aba0339a8e8241412a4790658
git <vojtawilson@gmail.com>
2021-07-01 20:46:38 +0200

    Improved readability

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from scraper import ScrapeBot
 
+# Try run
 my_url = "https://www.sreality.cz/hledani/pronajem/byty/praha?velikost=pokoj,1%2Bkk,1%2B1,2%2Bkk,2%2B1,3%2Bkk,3%2B1,4%2Bkk,4%2B1,5%2Bkk,5%2B1,6-a-vice,atypicky"
 newBot = ScrapeBot(my_url)
-newBot.startDriver(False)
+newBot.startDriver(True)
 newBot.scrape(1,1)
+
+# Extract data
+test_df = newBot.getData()
